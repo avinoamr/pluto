702319a284c1cd31381fa25d8de026ec5006212a
Roi Avinoam <avinoamr@gmail.com>
2017-03-27 21:25:05 +0300

    Working on re-renders

--- pluto.js
+++ pluto.js
@@ -17,26 +17,58 @@
             // because otherwise it will start firing custom-elements reactions
             // (createdCallback) before all of the attributes are set.
             var clone = this.cloneNode(true)
-            this._render(this.tokens, obj, clone.content)
+            // this._render(this.tokens, clone.content, obj)
+            // this._render(this.tokens, clone.content, obj)
 
             // NOTE: Yeah, it does mean that we clone it twice. Alternatively,
             // (1) we can opt to render on the template itself, but this will
             // reveal the rendered template and will make it impossible to make
             // dynamic changes and then re-compile. (2) we can create a clone
             // just once as using it a scratch space.
-            return document.importNode(clone.content, true)
+            var doc = document.importNode(clone.content, true)
+
+            this._render(this.tokens, doc, obj) // NOTE TEMPORARY.
+
+
+            doc.render = this._rerender.bind(this, this.tokens, doc)
+            return doc
         }
 
-        // actual rendering into an element or document fragment
-        _render(tokens, obj, into) {
-            var subtpls = []
+        _rerender(tokens, into, obj) {
+            var data = into.data
+            for (var i = 0 ; i < tokens.length ; i += 1) {
+                var t = tokens[i]
+                var el = data[t.path]
 
-            // first pass - subsitution
+                if (t.tpl) {
+                    el.render(obj)
+                    continue
+                }
+
+                var v = getPath(obj, t.name)
+                if (!t.attr) {
+                    el.textContent = v || ''
+                } else if (v === undefined) {
+                    el.removeAttribute(t.attr)
+                } else {
+                    el.setAttribute(t.attr, v)
+                }
+            }
+        }
+
+        _render(tokens, into, obj) {
+            var data = {}
+            var subdocs = []
             for (var i = 0 ; i < tokens.length ; i += 1) {
                 var t = tokens[i]
                 var el = getPath(into, t.path)
+                data[t.path] = el
+
                 if (t.tpl) {
-                    subtpls.push(el)
+                    var doc = pluto(el).render(obj)
+                    doc.el = el
+                    subdocs.push(doc)
+                    data[t.path] = doc
                     continue
                 }
 
@@ -50,13 +82,11 @@
                 }
             }
 
-            // TODO compute hard-links before messing up the elements order
+            subdocs.forEach(function (doc) {
+                into.insertBefore(doc, doc.el.nextSibling)
+            })
 
-            // second pass - sub-templates
-            for (var i = 0; i < subtpls.length; i += 1) {
-                var tpl = subtpls[i]
-                into.insertBefore(pluto(tpl).render(obj), tpl.nextSibling)
-            }
+            into.data = data
         }
 
         compile() {
@@ -131,8 +161,8 @@
 
         var path = Array.isArray(path) ? path : path.split('.')
         var v = obj
-        while (v !== undefined && path.length > 0) {
-            v = v[path.shift()]
+        for (var i = 0; v !== undefined && i < path.length; i += 1) {
+            v = v[path[i]]
         }
         return v
     }
