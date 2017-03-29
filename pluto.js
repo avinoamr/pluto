6eb099917b8d8d36c6c90be42fc49e2c0fe8e5a4
Roi Avinoam <avinoamr@gmail.com>
2017-03-29 03:17:34 +0300

    Restarting - this time with a constructor side-effect (urgh) that populates the document fragment once

--- pluto.js
+++ pluto.js
@@ -9,157 +9,26 @@
             : Object.setPrototypeOf(el, Template.prototype)
     }
 
-    // RenderFragment represents a single rendered template, along with its
-    // state and hard-links to the elements it generated and owns, in order
-    // to support re-rendering.
-    //
-    // NOTE we can't inherit directly from DocumentFragment. Instead, we use
-    // this intemediate object that handles the rendering, while only exposing
-    // its `render()` function on the returned DocumentFragment via `toDoc()`.
-    class RenderFragment {
-        constructor(tpl, tokens) {
-            this.tpl = tpl
-            this.tokens = tokens
-
-            this._repeat = tokenName(tpl.getAttribute('repeat'))
-        }
-
-        appendTo(el) {
-            this.children.forEach(function (child) {
-                (child instanceof RenderFragment)
-                    ? child.appendTo(el)
-                    : el.appendChild(child)
-            })
-            return el
-        }
-
-        _renderable(obj) {
-            obj.render = this.render.bind(this)
-            return obj
-        }
-
-        remove() {
-            this.children.forEach(function(child) {
-                child.remove()
-            })
-        }
-
-        renderRepeat(obj) {
-            var repeat = getPath(obj, this._repeat) || []
-            if (!this.doc || !this.children) {
-                this.placeholder = document.createElement('template')
-                this.placeholder.setAttribute('is', 'pluto-placeholder')
-                this.doc = new DocumentFragment()
-                this.doc.appendChild(this.placeholder)
-                this.children = []
-            }
-
-            // remove elements
-            while (this.children.length > repeat.length) {
-                this.children.pop().remove()
-            }
-
-            // add new elements
-            while (this.children.length < repeat.length) {
-                var child = new RenderFragment(this.tpl, this.tokens)
-                child._repeat = null
-                this.children.push(child)
-            }
-
-            repeat.forEach(function (item, idx) {
-                var child = this.children[idx]
-                obj.item = item
-                var doc = child.render(obj)
-                this.placeholder.parentNode.insertBefore(doc, this.placeholder)
-            }, this)
-
-            return this._renderable(this.doc)
-        }
-
-        render(obj) {
-            if (this._repeat) {
-                return this.renderRepeat(obj)
-            }
-
-            var tokens = this.tokens
-            if (!this.doc || !this.children) {
-                this.doc = this.tpl.cloneNode(true).content
-                this.children = [].map.call(this.doc.children, (child) => child)
-            }
-
-            this.paths = this.paths || {}
-            var subtpls = []
-            for (var i = 0 ; i < tokens.length ; i += 1) {
-                var t = tokens[i]
-                if (!this.paths[t.path]) {
-                    this.paths[t.path] = getPath(this, t.path)
-                }
-
-                var el = this.paths[t.path]
-                var v = getPath(obj, t.name)
-
-                // nested template
-                if (t.tpl) {
-                    subtpls.push(el)
-                    continue
-                }
-
-                if (!t.attr) {
-                    el.textContent = v || ''
-                } else if (v === undefined) {
-                    el.removeAttribute(t.attr)
-                } else {
-                    el.setAttribute(t.attr, v)
-                }
-            }
-
-            subtpls.forEach(function(tpl) {
-                if (!tpl._doc) {
-                    tpl._doc = pluto(tpl).render(obj)
-                    tpl.parentNode.replaceChild(tpl._doc, tpl)
-                } else {
-                    tpl._doc.render(obj)
-                }
-            }, this)
-
-            return this._renderable(this.doc)
-        }
-    }
-
     class Renderer {
-        constructor(tpl, tokens) {
-            this.tpl = tpl
-            this.tokens = tokens
+        constructor(tpl, doc) {
+            this.tokens = tpl.tokens
 
-            var doc = this.tpl.cloneNode(true).content
-            this.paths = this.tokens.reduce(function(paths, t) {
-                paths[t.path] = getPath(doc, t.path)
-                return paths
+            doc.appendChild(tpl.cloneNode(true).content)
+
+            // generate hard links from tokens to the generated elements in
+            // order to avoid re-computing them on every render.
+            this.paths = tpl.tokens.reduce(function (paths, t) {
+                return paths[t.path] = getPath(doc, t.path), paths
             }, {})
 
+            // copy the list of generated elements from the template in order
+            // to support removals
             this.children = [].map.call(doc.children, child => child)
-            this.doc = doc
-        }
-
-        replaceWith(el) {
-            var first = this.children.pop()
-            first.parentNode.replaceChild(el, first)
-            this.remove()
-        }
-
-        remove() {
-            while (this.children.length > 0) {
-                this.children.pop().remove()
-            }
         }
 
         render(obj) {
-            var doc = this.doc || null
-            this.doc = null
-
-            var tokens = this.tokens
-            for (var i = 0 ; i < tokens.length ; i += 1) {
-                var t = tokens[i]
+            for (var i = 0 ; i < this.tokens.length ; i += 1) {
+                var t = this.tokens[i]
                 var el = this.paths[t.path]
                 var v = getPath(obj, t.name)
 
@@ -176,81 +45,17 @@
                     el.setAttribute(t.attr, v)
                 }
             }
-
-            return doc
         }
-    }
-
-    class IfRenderer {
-        constructor(tpl, tokens, if_) {
-            this.tpl = tpl
-            this.tokens = tokens
-            this.if = if_
-        }
-
-        render(obj) {
-            var if_ = getPath(obj, this.if)
-
-            var doc
-            if (!this.placeholder) {
-                this.placeholder = placeholder()
-                doc = new DocumentFragment()
-                doc.appendChild(this.placeholder)
-            }
-
-            if (!if_ && this.child) {
-                this.child = this.child.replaceWith(this.placeholder)
-            } else if (if_ && !this.child) {
-                this.child = new Renderer(this.tpl, this.tokens)
-                var subdoc = this.child.render(obj)
-                this.placeholder.replaceWith(subdoc)
-            }
-
-            return doc
-        }
-
     }
 
     class Template extends HTMLTemplateElement {
-
-        setAttribute(k, v) {
-            HTMLTemplateElement.prototype.setAttribute.apply(this, arguments)
-
-            if (k === 'repeat') {
-                this._repeat = v
-            }
-        }
-
         render(obj) {
             this.compile()
 
-            var renderer
-            if (this.repeat) {
-                renderer = new RepeatRenderer(this, this.tokens, this.repeat)
-            } else if (this.if) {
-                renderer = new IfRenderer(this, this.tokens, this.if)
-            } else {
-                renderer = new Renderer(this, this.tokens)
-            }
-
             var doc = new DocumentFragment()
-            doc.render = function(obj) {
-                var appendDoc = renderer.render(obj)
-
-                // move the new elements to the final doc
-                while (appendDoc && appendDoc.children.length > 0) {
-                    doc.appendChild(appendDoc.children[0])
-                }
-
-                return this
-            }
-
+            var renderer = new Renderer(this, doc)
+            doc.render = (obj) => (renderer.render(obj), doc)
             return doc.render(obj)
-        }
-
-        renderer() {
-            this.compile()
-            return new RenderFragment(this, this.tokens)
         }
 
         compile() {
@@ -297,7 +102,6 @@
     }
 
     pluto.Template = Template
-    pluto.RenderFragment = RenderFragment
 
     // -- HELPER FUNCTIONS
 
