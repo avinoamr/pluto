6d8a91cc82257df9d1093d190f612308fe8a1197
Roi Avinoam <avinoamr@gmail.com>
2017-04-05 23:01:37 +0300

    Re-order the classes to keep the most important one (Template) at the top

--- pluto.js
+++ pluto.js
@@ -17,217 +17,6 @@
             : Object.setPrototypeOf(el, Template.prototype)
     }
 
-    class Renderer {
-        constructor(tpl) {
-            this.tpl = tpl
-            this.tokens = tpl.tokens
-        }
-
-        remove() {
-            while (this.children.length > 0) {
-                this.children.pop().remove()
-            }
-        }
-
-        init(obj) {
-            var doc = this.tpl.cloneNode(true).content
-
-            // remove attributes or tokens before importing in order to hide
-            // the placeholder tokens from the constructor of custom elements.
-            this.tokens.forEach(function(t) {
-                var el = getPath(doc, t.path)
-                if (!t.attr) {
-                    el.textContent = ''
-                } else {
-                    el.removeAttribute(t.attr)
-                }
-            })
-
-            doc = document.importNode(doc, true) // this will upgrade elements
-            doc.render = (obj) => (this.render(obj), doc)
-            doc.remove = () => this.remove()
-
-            // generate hard links from tokens to the generated elements in
-            // order to avoid re-computing them on every render.
-            this.paths = this.tokens.reduce(function (paths, t, idx) {
-                var el = getPath(doc, t.path)
-                if (t.tpl) {
-                    var subdoc = pluto(el).render(obj)
-                    el.replaceWith(subdoc)
-                    el = subdoc
-                }
-
-                paths[idx] = { el }
-
-                // marke observed attributes
-                if (t.attr && el.attributeChangedCallback) {
-                    var observed = el.constructor.observedAttributes || [];
-                    paths[idx].observed = observed.indexOf(t.attr) !== -1
-                }
-
-                return paths
-            }, {})
-
-            // copy the list of generated elements from the template in order
-            // to support removals
-            this.children = [].map.call(doc.children, child => child)
-
-            return doc
-        }
-
-        render(obj) {
-            if (!this._doc) {
-                this._doc = this.init(obj)
-            }
-
-            for (var i = 0 ; i < this.tokens.length ; i += 1) {
-                var t = this.tokens[i]
-                var { el, observed, listener } = this.paths[i]
-                var v = getPath(obj, t.name)
-
-                // nested template
-                if (t.tpl) {
-                    el.render(obj)
-                    continue
-                }
-
-                if (!t.attr) {
-                    el.textContent = v || ''
-                } else if (v === undefined) {
-                    el.removeAttribute(t.attr)
-                } else if (typeof v === 'function' && t.attr.startsWith('on')) {
-                    // set the event listener if it has changed
-                    var evName = t.attr.slice(2)
-                    if (listener) {
-                        el.removeEventListener(evName, listener)
-                    }
-
-                    v = v._bound || v
-                    el.addEventListener(evName, v)
-                    this.paths[i].listener = v // remember it for next render
-                } else if (typeof v !== 'string' && observed) {
-                    el.attributeChangedCallback(t.attr, null, v, null)
-                } else {
-                    if (t.attr === 'class' && typeof v === 'object') {
-                        if (Array.isArray(v)) {
-                            return v.join(' ')
-                        }
-
-                        v = Object.keys(v).filter(function (k) {
-                            return v[k]
-                        }).join(' ')
-                    } else if (t.attr === 'style' && typeof v === 'object') {
-                        v = Object.keys(v).map(function(k) {
-                            return k + ': ' + v
-                        }).join('; ')
-                    }
-
-                    v = v.toString()
-                    if (v === '[object Object]') {
-                        el.removeAttribute(t.attr)
-                    } else {
-                        el.setAttribute(t.attr, v)
-                    }
-                }
-            }
-
-            return this._doc
-        }
-    }
-
-    class RepeatRenderer {
-        constructor(tpl) {
-            this.tpl = tpl
-            this.children = []
-            this.repeat = tpl.repeat
-        }
-
-        remove() {
-            while (this.children.length > 0) {
-                this.children.pop().remove()
-            }
-        }
-
-        init() {
-            var doc = new DocumentFragment()
-            doc.render = (obj) => (this.render(obj), doc)
-            doc.remove = () => this.remove()
-
-            this.placeholder = placeholder()
-            doc.appendChild(this.placeholder)
-
-            return doc
-        }
-
-        render(obj) {
-            if (!this._doc) {
-                this._doc = this.init()
-            }
-
-            var item = obj.item
-            var items = getPath(obj, this.repeat) || []
-
-            // remove obsolete items
-            while (this.children.length > items.length) {
-                this.children.pop().remove()
-            }
-
-            // update existing items
-            for (var i = 0; i < this.children.length; i += 1) {
-                obj.item = items[i]
-                this.children[i].render(obj)
-            }
-
-            // create new items
-            while (this.children.length < items.length) {
-                var i = this.children.length
-                obj.item = items[i]
-                var doc = new Renderer(this.tpl).render(obj)
-                this.children.push(doc)
-                this.placeholder.before(doc)
-            }
-
-            obj.item = item // restore previous item value.
-            return this._doc
-        }
-    }
-
-    class CondRenderer {
-        constructor(tpl) {
-            this.tpl = tpl
-            this.child = null
-            this.cond = tpl.cond
-        }
-
-        init() {
-            var doc = new DocumentFragment()
-            doc.render = (obj) => (this.render(obj), doc)
-
-            this.placeholder = placeholder()
-            doc.appendChild(this.placeholder)
-            return doc
-        }
-
-        render(obj) {
-            if (!this._doc) {
-                this._doc = this.init()
-            }
-
-            var cond = getPath(obj, this.cond) || false
-            if (cond && this.child) {
-                this.child.render(obj)
-            } else if (cond && !this.child) {
-                var doc = new Renderer(this.tpl).render(obj)
-                this.placeholder.before(doc)
-                this.child = doc
-            } else if (!cond && this.child) {
-                this.child = this.child.remove()
-            }
-
-            return this._doc
-        }
-    }
-
     class Template extends HTMLTemplateElement {
         render(obj) {
             this.compile()
@@ -347,6 +136,221 @@
                 console.warn(err.message, 'in: {{', expr, '}}')
                 return function () {}
             }
+        }
+    }
+
+    class Renderer {
+        constructor(tpl) {
+            this.tpl = tpl
+            this.tokens = tpl.tokens
+        }
+
+        remove() {
+            while (this.children.length > 0) {
+                this.children.pop().remove()
+            }
+        }
+
+        init(obj) {
+            var doc = this.tpl.cloneNode(true).content
+
+            // remove attributes or tokens before importing in order to hide
+            // the placeholder tokens from the constructor of custom elements.
+            this.tokens.forEach(function(t) {
+                var el = getPath(doc, t.path)
+                if (!t.attr) {
+                    el.textContent = ''
+                } else {
+                    el.removeAttribute(t.attr)
+                }
+            })
+
+            doc = document.importNode(doc, true) // this will upgrade elements
+            doc.render = (obj) => (this.render(obj), doc)
+            doc.remove = () => this.remove()
+
+            // generate hard links from tokens to the generated elements in
+            // order to avoid re-computing them on every render.
+            this.paths = this.tokens.reduce(function (paths, t, idx) {
+                var el = getPath(doc, t.path)
+                if (t.tpl) {
+                    var subdoc = pluto(el).render(obj)
+                    el.replaceWith(subdoc)
+                    el = subdoc
+                }
+
+                paths[idx] = { el }
+
+                // marke observed attributes
+                if (t.attr && el.attributeChangedCallback) {
+                    var observed = el.constructor.observedAttributes || [];
+                    paths[idx].observed = observed.indexOf(t.attr) !== -1
+                }
+
+                return paths
+            }, {})
+
+            // copy the list of generated elements from the template in order
+            // to support removals
+            this.children = [].map.call(doc.children, child => child)
+
+            return doc
+        }
+
+        render(obj) {
+            if (!this._doc) {
+                this._doc = this.init(obj)
+            }
+
+            for (var i = 0 ; i < this.tokens.length ; i += 1) {
+                var t = this.tokens[i]
+                var { el, observed, listener } = this.paths[i]
+                var v = getPath(obj, t.name)
+
+                // nested template
+                if (t.tpl) {
+                    el.render(obj)
+                    continue
+                }
+
+                if (!t.attr) {
+                    el.textContent = v || ''
+                } else if (typeof v === 'function' && t.attr.startsWith('on')) {
+                    // set the event listener if it has changed
+                    var evName = t.attr.slice(2)
+                    if (evName[0] === '-') { // "on-"
+                        evName = evName.slice(1)
+                    }
+
+                    if (listener) {
+                        el.removeEventListener(evName, listener)
+                    }
+
+                    v = v._bound || v
+                    el.addEventListener(evName, v)
+                    this.paths[i].listener = v // remember it for next render
+                } else if (v === undefined) {
+                    el.removeAttribute(t.attr)
+                } else if (typeof v !== 'string' && observed) {
+                    el.attributeChangedCallback(t.attr, null, v, null)
+                } else {
+                    if (t.attr === 'class' && typeof v === 'object') {
+                        if (Array.isArray(v)) {
+                            return v.join(' ')
+                        }
+
+                        v = Object.keys(v).filter(function (k) {
+                            return v[k]
+                        }).join(' ')
+                    } else if (t.attr === 'style' && typeof v === 'object') {
+                        v = Object.keys(v).map(function(k) {
+                            return k + ': ' + v[k]
+                        }).join('; ')
+                    }
+
+                    v = v.toString()
+                    if (v === '[object Object]') {
+                        el.removeAttribute(t.attr)
+                    } else {
+                        el.setAttribute(t.attr, v)
+                    }
+                }
+            }
+
+            return this._doc
+        }
+    }
+
+    class RepeatRenderer {
+        constructor(tpl) {
+            this.tpl = tpl
+            this.children = []
+            this.repeat = tpl.repeat
+        }
+
+        remove() {
+            while (this.children.length > 0) {
+                this.children.pop().remove()
+            }
+        }
+
+        init() {
+            var doc = new DocumentFragment()
+            doc.render = (obj) => (this.render(obj), doc)
+            doc.remove = () => this.remove()
+
+            this.placeholder = placeholder()
+            doc.appendChild(this.placeholder)
+
+            return doc
+        }
+
+        render(obj) {
+            if (!this._doc) {
+                this._doc = this.init()
+            }
+
+            var item = obj.item
+            var items = getPath(obj, this.repeat) || []
+
+            // remove obsolete items
+            while (this.children.length > items.length) {
+                this.children.pop().remove()
+            }
+
+            // update existing items
+            for (var i = 0; i < this.children.length; i += 1) {
+                obj.item = items[i]
+                this.children[i].render(obj)
+            }
+
+            // create new items
+            while (this.children.length < items.length) {
+                var i = this.children.length
+                obj.item = items[i]
+                var doc = new Renderer(this.tpl).render(obj)
+                this.children.push(doc)
+                this.placeholder.before(doc)
+            }
+
+            obj.item = item // restore previous item value.
+            return this._doc
+        }
+    }
+
+    class CondRenderer {
+        constructor(tpl) {
+            this.tpl = tpl
+            this.child = null
+            this.cond = tpl.cond
+        }
+
+        init() {
+            var doc = new DocumentFragment()
+            doc.render = (obj) => (this.render(obj), doc)
+
+            this.placeholder = placeholder()
+            doc.appendChild(this.placeholder)
+            return doc
+        }
+
+        render(obj) {
+            if (!this._doc) {
+                this._doc = this.init()
+            }
+
+            var cond = getPath(obj, this.cond) || false
+            if (cond && this.child) {
+                this.child.render(obj)
+            } else if (cond && !this.child) {
+                var doc = new Renderer(this.tpl).render(obj)
+                this.placeholder.before(doc)
+                this.child = doc
+            } else if (!cond && this.child) {
+                this.child = this.child.remove()
+            }
+
+            return this._doc
         }
     }
 
